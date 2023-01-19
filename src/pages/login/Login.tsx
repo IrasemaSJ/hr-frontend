3bceeb2f9364ad45dee861f66f4b0b4d0a5edd5f
Carlos Diaz <carlosdiaz.dev@gmail.com>
2023-01-19 00:37:57 -0600

    feat: :sparkles: add complete login view and install a svgr dependency for svg

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
-import './Login.css'
-
+import './Login.css';
+import { ReactComponent as ImprovingLogo } from '../../assets/logo_improving.svg';
+import { ReactComponent as SignIn } from '../../assets/sign_in_ms.svg';
+import { useNavigate } from 'react-router-dom';
 
 export const Login = () => {
+  const navigate = useNavigate();
   return (
-    <>
-      <div className="login-general">
-        <div className="login-container">
-          
-        </div>
+    <div className="login-general">
+      <div className="login-container">
+        <ImprovingLogo className="login-logotype" />
+        <p className="login-text">Welcome to HR System</p>
+        <button className="login-button" onClick={() => navigate('/employees')}>
+          <SignIn />
+        </button>
       </div>
-    </>
+    </div>
   );
 };
