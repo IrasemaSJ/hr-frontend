e79ad233a016036da044f8160c8b91dcdf522815
IrasemaSJ <Irasema.Serrano@improving.com>
2023-02-01 14:50:56 -0600

    feat: :sparkles: add information to holiday module

--- src/pages/holidays/Holidays.tsx
+++ src/pages/holidays/Holidays.tsx
@@ -1,7 +1,79 @@
-import React from 'react'
+import Table, { ColumnsType } from 'antd/es/table';
+
+interface DataType {
+  key: string;
+  publicholiday: string;
+  date: string; //type Date
+  allusiveimage: string;
+}
+
+const columns: ColumnsType<DataType> = [
+  {
+    title: 'Public holiday',
+    dataIndex: 'publicholiday',
+    align: 'center',
+  },
+  {
+    title: 'Date',
+    dataIndex: 'date',
+    align: 'center',
+  },
+  {
+    title: 'Allusive image',
+    dataIndex: 'allusiveimage',
+    align: 'center',
+  },
+];
+
+const data: DataType[] = [
+  {
+    key: '1',
+    publicholiday: 'MEXICAN CONSTITUTION DAY',
+    date: '16th February',
+    allusiveimage: 'imagen',
+  },
+  {
+    key: '2',
+    publicholiday: 'BENITO JUAREZ DAY',
+    date: '20th March',
+    allusiveimage: 'imagen',
+  },
+  {
+    key: '3',
+    publicholiday: 'HOLI FRIDAY',
+    date: '7th April',
+    allusiveimage: 'imagen',
+  },
+  {
+    key: '4',
+    publicholiday: 'LABOR DAY',
+    date: '1st May',
+    allusiveimage: 'imagen',
+  },
+  {
+    key: '5',
+    publicholiday: 'MEXICAN REVOLUTION DAY',
+    date: '20th November',
+    allusiveimage: 'imagen',
+  },
+  {
+    key: '6',
+    publicholiday: 'CHRISTMAS DAY',
+    date: '25th December',
+    allusiveimage: 'imagen',
+  },
+];
 
 export const Holidays = () => {
   return (
-    <div>Holidays</div>
-  )
-}
+    <>
+      <h1>Official Holidays</h1>
+      <Table
+        columns={columns}
+        dataSource={data}
+        style={{ marginTop: '20px' }}
+        pagination={{ hideOnSinglePage: true }}
+      />
+    </>
+  );
+};
