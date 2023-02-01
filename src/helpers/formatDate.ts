f5e6a69b025b7bcd9d18df29a616028e319968cf
Adranuz <60374995+adranuz@users.noreply.github.com>
2023-02-01 13:59:20 -0600

    fix: :bug: change functional format to an object

--- src/helpers/formatDate.ts
+++ src/helpers/formatDate.ts
@@ -1,8 +1,14 @@
 import * as dayjs from 'dayjs';
 
+export const format = {
+  table: 'ddd, DD MMM YYYY',
+  input: 'DD MMM YYYY',
+  post: 'YYYY-MM-DD',
+};
+
 export const formatTableDate = (date: string | dayjs.Dayjs) => {
   if (typeof date === 'string') {
     date = dayjs(date);
   }
-  return date.format('ddd, DD MMM YYYY');
+  return date.format(format.table);
 };
