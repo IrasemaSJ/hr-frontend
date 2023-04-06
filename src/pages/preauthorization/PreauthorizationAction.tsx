0e2495940f2b645c185d2bd6a274f7a5114c0e01
Adranuz <60374995+adranuz@users.noreply.github.com>
2023-04-06 13:41:57 -0600

    refactor: :construction_worker: send request to approve a contingecy by preauthorizaion

--- src/pages/preauthorization/PreauthorizationAction.tsx
+++ src/pages/preauthorization/PreauthorizationAction.tsx
@@ -14,6 +14,8 @@ import { ReactComponent as Logo } from '../../assets/logo_improving.svg';
 import { useSearchParams } from 'react-router-dom';
 import jwt_decode from 'jwt-decode';
 import ApiHR from '../../api/ApiHR';
+import { PreauthorizationForm } from './PreauthorizationForm';
+import { PreauthorizationResult } from './PreauthorizationResult';
 
 //simulacion de manejador de peticiones
 function apiReqest(body: any, delay: number) {
@@ -68,18 +70,16 @@ export const PreauthorizationAction = () => {
       });
 
       console.log(response);
-      setResult({ ...postBody, message: response });
+      setResult({
+        ...postBody,
+        message: response.data,
+        statusCode: response.status,
+      });
     } catch (error) {
       console.log(error);
       setResult(error);
     }
     setIsLoading(false);
-  };
-
-  const handlerClose = () => {
-    window.opener = null;
-    window.open('', '_self');
-    window.close();
   };
 
   return (
@@ -119,157 +119,18 @@ export const PreauthorizationAction = () => {
             </Typography.Title>
             <Divider />
             {!result ? (
-              <MyForm handleFinish={handleFinish} folio={folio} />
+              <PreauthorizationForm handleFinish={handleFinish} folio={folio} />
             ) : (
-              <>
-                <Result
-                  status={result.statusCode <= 300 ? 'success' : 'error'}
-                  title={
-                    result.statusCode <= 300
-                      ? 'Successfully Submitted!'
-                      : 'Submission Failed'
-                  }
-                  subTitle={
-                    result.statusCode <= 300 ? (
-                      <>
-                        <Typography.Text>
-                          Request {result.folio} has been {result.status}
-                        </Typography.Text>
-                        <Typography.Paragraph>
-                          Observations: {result.observations || 'None'}
-                        </Typography.Paragraph>
-                      </>
-                    ) : (
-                      <Typography.Paragraph>
-                        Something went wrong, please try again later
-                      </Typography.Paragraph>
-                    )
-                  }
-                />
-                <Button type="primary" key="button" onClick={handlerClose}>
-                  Close
-                </Button>
-              </>
+              <PreauthorizationResult
+                folio={result.folio}
+                observations={result.observations || 'None'}
+                statusCode={result.statusCode}
+                status={result.status}
+              />
             )}
           </div>
         </Card>
       </Spin>
     </div>
-  );
-};
-
-const MyForm: React.FC<{
-  handleFinish: (values: any) => void;
-  folio: string;
-}> = ({ handleFinish, folio }) => {
-  const [form] = Form.useForm();
-  const status = {
-    positive: 'approved',
-    negative: 'rejected',
-  };
-  const [modal, contextHolder] = Modal.useModal();
-
-  // when declined, observations message is required
-  const handleDecline = async () => {
-    form.setFieldValue('status', status.negative);
-    const values = { ...form.getFieldsValue(), status: status.negative };
-    await form.validateFields();
-    modal.confirm({
-      title: 'Confirm Rejection',
-      content: `Are you sure you want to reject this request?`,
-      okText: 'Reject',
-      okType: 'danger',
-      onOk: () => {
-        handleFinish({ ...values });
-      },
-    });
-  };
-
-  // when approved, observations message is not required
-  const handleAccept = () => {
-    const values = { ...form.getFieldsValue(), status: status.positive };
-    modal.confirm({
-      title: 'Confirm Approval',
-      content: `Are you sure you want to approve this request?`,
-      okText: 'Approve',
-      onOk: () => {
-        handleFinish({ ...values });
-      },
-    });
-  };
-
-  return (
-    <>
-      <Typography.Text>
-        The request <b>{folio}</b> need your review
-      </Typography.Text>
-      <Form
-        form={form}
-        layout="vertical"
-        name="authorize"
-        style={{ width: '100%', margin: '2rem' }}
-      >
-        <Form.Item
-          name="observations"
-          label="Observations"
-          colon={true}
-          validateFirst={false}
-          rules={[
-            {
-              /** observations is required when status is decline
-               * Only onClick of
-               *
-               * Status is blanked when user focus on observations input
-               */
-              async validator(rule, value) {
-                if (
-                  form.getFieldValue('status') === status.negative &&
-                  !value
-                ) {
-                  throw new Error(
-                    "Please, for decline input the request's observations!",
-                  );
-                }
-              },
-            },
-          ]}
-        >
-          <Input.TextArea
-            placeholder="Observations"
-            onFocus={() => {
-              form.setFieldValue('status', '');
-            }}
-            style={{ height: 120, marginBottom: 24, resize: 'none', margin: 0 }}
-          />
-        </Form.Item>
-        <Form.Item>
-          <div style={{ display: 'flex', justifyContent: 'space-around' }}>
-            <Button
-              danger
-              type="primary"
-              htmlType="button"
-              onClick={handleDecline}
-            >
-              Decline
-            </Button>
-            <Button
-              type="primary"
-              htmlType="button"
-              onClick={() => {
-                form.setFieldValue('status', '');
-                handleAccept();
-              }}
-            >
-              Authorize
-            </Button>
-          </div>
-        </Form.Item>
-      </Form>
-      <Typography.Text type="secondary">
-        For more information enter your panel in the system or check your email
-        again.
-      </Typography.Text>
-      {contextHolder}
-    </>
   );
 };
---
+++ src/pages/preauthorization/PreauthorizationForm.tsx
@@ -0,0 +1,118 @@
+import { Button, Form, Input, Modal, Typography } from 'antd';
+import React from 'react';
+
+export const PreauthorizationForm: React.FC<{
+  handleFinish: (values: any) => void;
+  folio: string;
+}> = ({ handleFinish, folio }) => {
+  const [form] = Form.useForm();
+  const status = {
+    positive: 'approved',
+    negative: 'rejected',
+  };
+  const [modal, contextHolder] = Modal.useModal();
+
+  // when declined, observations message is required
+  const handleDecline = async () => {
+    form.setFieldValue('status', status.negative);
+    const values = { ...form.getFieldsValue(), status: status.negative };
+    await form.validateFields();
+    modal.confirm({
+      title: 'Confirm Rejection',
+      content: `Are you sure you want to reject this request?`,
+      okText: 'Reject',
+      okType: 'danger',
+      onOk: () => {
+        handleFinish({ ...values });
+      },
+    });
+  };
+
+  // when approved, observations message is not required
+  const handleAccept = () => {
+    const values = { ...form.getFieldsValue(), status: status.positive };
+    modal.confirm({
+      title: 'Confirm Approval',
+      content: `Are you sure you want to approve this request?`,
+      okText: 'Approve',
+      onOk: () => {
+        handleFinish({ ...values });
+      },
+    });
+  };
+
+  return (
+    <>
+      <Typography.Text>
+        The request <b>{folio}</b> need your review
+      </Typography.Text>
+      <Form
+        form={form}
+        layout="vertical"
+        name="authorize"
+        style={{ width: '100%', margin: '2rem' }}
+      >
+        <Form.Item
+          name="observations"
+          label="Observations"
+          colon={true}
+          validateFirst={false}
+          rules={[
+            {
+              /** observations is required when status is decline
+               * Only onClick of
+               *
+               * Status is blanked when user focus on observations input
+               */
+              async validator(rule, value) {
+                if (
+                  form.getFieldValue('status') === status.negative &&
+                  !value
+                ) {
+                  throw new Error(
+                    "Please, for decline input the request's observations!",
+                  );
+                }
+              },
+            },
+          ]}
+        >
+          <Input.TextArea
+            placeholder="Observations"
+            onFocus={() => {
+              form.setFieldValue('status', '');
+            }}
+            style={{ height: 120, marginBottom: 24, resize: 'none', margin: 0 }}
+          />
+        </Form.Item>
+        <Form.Item>
+          <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+            <Button
+              danger
+              type="primary"
+              htmlType="button"
+              onClick={handleDecline}
+            >
+              Decline
+            </Button>
+            <Button
+              type="primary"
+              htmlType="button"
+              onClick={() => {
+                form.setFieldValue('status', '');
+                handleAccept();
+              }}
+            >
+              Authorize
+            </Button>
+          </div>
+        </Form.Item>
+      </Form>
+      <Typography.Text type="secondary">
+        For more information enter your panel in the system or check your email
+        again.
+      </Typography.Text>
+      {contextHolder}
+    </>
+  );
+};
